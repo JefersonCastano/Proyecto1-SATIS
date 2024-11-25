03ee55bca228f9fa8aaa64942ff6319c45f7f102
Jeferson <jcastanoossa@unicauca.edu.co>
2024-11-24 19:51:00 -0500

    Add test css

--- indicador-dashlet-master/indicadores/indicador.test.class.php
+++ indicador-dashlet-master/indicadores/indicador.test.class.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'indicador.class.php';
-    use Combodo\iTop\Application\UI\Base\Component\Panel\PanelUIBlockFactory;
+    use Combodo\iTop\Application\UI\Base\Component\Dashlet\DashletContainer;
 
     class IndicadorTest extends Indicador {
 
@@ -9,7 +9,12 @@
 
         public function Render($oPage, $bEditMode = false, $aExtraParams = array()) {
 
-            $oPanel = PanelUIBlockFactory::MakeForInformation('Indicador de Test', 'Indicador de Test');
+            // Incluir el archivo CSS
+            $sCSSFile = utils::GetAbsoluteUrlModulesRoot() . 'indicador-dashlet-master/asset/css/dashlet-indicador.css';
+            $oPage->add_linked_stylesheet($sCSSFile);
+
+            $oPanel = new DashletContainer('dashlet-indicador');
+            $oPanel->AddCSSClass('dashlet-indicador');
             $oPanel->AddHtml('<p>Este es un indicador de test, puedo insertar código HTML libremente.</p>');
             $oPanel->AddHtml('<p>Como por ejemplo, James Rodriguez.</p>');
             $oPanel->AddHtml('<img src="https://pbs.twimg.com/media/EkQ1PpSU4AU7XG3.jpg" alt="Descripción de la imagen" />');
